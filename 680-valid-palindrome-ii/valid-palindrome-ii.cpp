aba6090b3d5a6b69c4b40c9d5c6a1d3565d02c39
Lakshay <121056694+lakshaybishnoi@users.noreply.github.com>
2025-05-26 12:19:46 +0530

    Time: 6 ms (40.33%) | Memory: 26.8 MB (22.71%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPalindrome(string s, int low, int high) {
+        while (low <= high) {
+            if (s[low] != s[high]) return false;
+            low++;
+            high--;
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        int n = s.size();
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+            if (s[low] == s[high]) {
+                low++;
+                high--;
+            } else {
+                 
+                return isPalindrome(s, low + 1, high) || isPalindrome(s, low, high - 1);
+            }
+        }
+
+        return true;
+    }
+};
